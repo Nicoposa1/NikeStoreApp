284fb56c9abf1b48c4efa5dc656a281189578cd2
Nicolas Posa <nicoposa57@gmail.com>
2024-04-23 00:14:54 -0300

    shopping cart screen and separete component

--- client/src/screens/ShoppingCartScreen.tsx
+++ client/src/screens/ShoppingCartScreen.tsx
@@ -1,18 +1,87 @@
-import {FlatList, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Text,
+  FlatList,
+  View,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 import cart from '../data/cart';
-import {CartListItem} from '../components/CartListItem';
+import { CartListItem } from '../components/CartListItem';
+
+const ShoppingCartTotals = () => {
+  return (
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>420,00 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>10,00 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>430,00 US$</Text>
+      </View>
+    </View>
+  );
+}
+
 
 export const ShoppingCartScreen = () => {
+
   return (
-    <View>
+    <>
       <FlatList
         data={cart}
-        renderItem={({item}) => <CartListItem cartItem={item} />}
-        keyExtractor={item => item.product.id}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={<ShoppingCartTotals />}
       />
-    </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>
+          Checkout
+        </Text>
+      </Pressable>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  totalsContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: 'gainsboro',
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+
+  button: {
+    position: 'absolute',
+    backgroundColor: 'black',
+    bottom: 30,
+    width: '90%',
+    alignSelf: 'center',
+    padding: 20,
+    borderRadius: 100,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16,
+  },
+});
