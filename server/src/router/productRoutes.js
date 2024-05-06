59d4b53c57fff3d4aa3ce295fafe786ed5a914a4
Nicolas Posa <nicoposa57@gmail.com>
2024-05-06 00:20:42 -0300

    handling errors

--- server/src/router/productRoutes.js
+++ server/src/router/productRoutes.js
@@ -7,8 +7,17 @@ router.get("/", async (req, res) => {
   res.send({ status: "OK", data: products });
 });
 
-router.get("/:productId", (req, res) => {
-  res.send(`Get product with id ${req.params.productId}`);
+router.get("/:productId", async (req, res) => {
+  try {
+    const product = await getOneProduct(req.params.productId);
+    if (!product) {
+      res.status(404).send({ status: "error", message: "Product not found" });
+      return;
+    }
+    res.send({ status: "OK", data: product });
+  } catch (error) {
+    res.send({ status: "error", message: error.message });
+  }
 });
 
 module.exports = router;
